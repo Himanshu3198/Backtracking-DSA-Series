e47419ddb5a50c74401f5cca35a55d90f1756cc0
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-05-10 13:07:51 +0530

    Update backtracking-216. Combination Sum III.cpp

--- backtracking-216. Combination Sum III.cpp
+++ backtracking-216. Combination Sum III.cpp
@@ -45,3 +45,31 @@ public:
         
     }
 };
+//   method 2 
+#define ll long long int
+class Solution {
+public:
+    vector<vector<int>>res;
+    void solve(int k,int n,int num,ll sum,vector<int>&v){
+        if(v.size()==k){
+            if(sum==n){
+                res.push_back(v);
+            }
+             return;
+        }
+        if(sum>=n or num>9) return; 
+        
+        v.push_back(num);
+        solve(k,n,num+1,sum+num,v);
+        if(v.size()>0)
+        v.pop_back();
+        solve(k,n,num+1,sum,v);
+      
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+     
+        vector<int>v;
+        solve(k,n,1,0L,v);
+        return res;
+    }
+};
