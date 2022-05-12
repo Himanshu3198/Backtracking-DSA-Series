92f79c0351e07dd13c04b89d755d04de7b437452
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-05-12 23:59:52 +0530

    Update Backtracking-Permutation-ii.cpp

--- Backtracking-Permutation-ii.cpp
+++ Backtracking-Permutation-ii.cpp
@@ -1,35 +1,30 @@
-permutaion  II
-
-
-
 class Solution {
 public:
-    
     vector<vector<int>>res;
-    
-    void solve(vector<int>&nums,int idx){
-        
-        if(idx==nums.size()){
-            
-            res.push_back(nums);
-            return;
-        }
-          unordered_set<int>st;
-        for(int i=idx;i<nums.size();i++){
-            
-           if(st.find(nums[i])!=st.end()==true){
-               continue;
-           }
-              st.insert(nums[i]);
-           swap(nums[i],nums[idx]);
-            solve(nums,idx+1);
-            swap(nums[i],nums[idx]);
-        }
-    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-      
- 
-        solve(nums,0);
+        
+        set<vector<int>>st;
+        vector<vector<int>>res;
+        res.push_back(nums);
+        st.insert(nums);
+        
+        for(int i=0;i<nums.size();i++){
+            
+            for(int j=0;j<nums.size();j++){
+                
+                if(i==j) continue;
+                
+                swap(nums[i],nums[j]);
+                if(!st.count(nums)){
+                    st.insert(nums);
+                    res.push_back(nums);
+                
+                }
+                swap(nums[i],nums[j]);
+                
+            }
+        }
         return res;
+         
     }
 };
