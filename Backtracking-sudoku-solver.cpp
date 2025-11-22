f3c4dac4691565464ee129428281dc4e76af3236
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2025-11-22 16:46:26 +0530

    Add Java version of the Sudoku solver

--- Backtracking-sudoku-solver.cpp
+++ Backtracking-sudoku-solver.cpp
@@ -63,3 +63,63 @@ public:
         
     }
 };
+
+
+// java version of this
+
+class Solution {
+
+    private Boolean isValid(char [][] board,int row,int col, char c){
+   
+        for(int i=0;i<9;i++){
+
+            if(board[i][col] == c) return false;
+            if(board[row][i] == c) return false;
+
+            int r = 3*(row/3) + i/3;
+            int cc = 3*(col/3) + i%3;
+
+            if(board[r][cc] == c) return false;
+        }
+
+        return true;
+    }
+    private Boolean solve(char [][] board){
+                
+                int n = board.length;
+
+                for(int i=0;i<n;i++){
+                    for(int j=0; j<n;j++){
+
+                        if(board[i][j] == '.'){
+
+                            for(char c = '1'; c<='9';c++){
+                                
+                                if(isValid(board,i,j,c))
+                                    {
+                                    
+                                      board[i][j] = c;
+                                      if(solve(board) == true){
+                                         return true;
+                                    }
+                                    else
+                                    {
+                                        board[i][j] = '.';
+                                    }
+                                }
+                              
+                            }
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+        
+    }
+
+    public void solveSudoku(char[][] board) {
+
+        solve(board);
+    }
+}
