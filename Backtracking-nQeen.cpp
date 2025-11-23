bdc4cbefbfd1f9362c38246b02015fda62f99341
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2025-11-23 16:06:34 +0530

    Refactor N-Queens solution with Java version

--- Backtracking-nQeen.cpp
+++ Backtracking-nQeen.cpp
@@ -39,3 +39,84 @@ public:
         return ret;
     }
 };
+
+// java version
+class Solution {
+
+    private Boolean isValid(List<List<Character>> board, int row, int col, int n) {
+
+        // check in col
+        for (int r = 0; r <row; r++) {
+
+            if (board.get(r).get(col) == 'Q')
+                return false;
+        }
+
+        // left up diagonal
+
+        for (int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
+
+            // if(board[r][c] == 'Q') return false;  
+            if (board.get(r).get(c) == 'Q')
+                return false;
+        }
+
+        // right up diagonal
+
+        for (int r = row, c = col; r >= 0 && c < n; r--, c++) {
+
+            // if(board[r][c] == 'Q') return false;  
+            if (board.get(r).get(c) == 'Q')
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public Boolean dfs(List<List<Character>> board, List<List<String>> res, int row, int n) {
+
+        if (row == n) {
+            List<String> solution = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    sb.append(board.get(i).get(j));
+                }
+                solution.add(sb.toString());
+            }
+            res.add(solution);
+            return true;
+        }
+        for (int col = 0; col < n; col++) {
+
+              if (isValid(board, row, col, n)) {
+
+                board.get(row).set(col, 'Q');
+                dfs(board, res, row + 1, n);
+                board.get(row).set(col, '.');
+            }
+
+        }
+        return false;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+
+        List<List<Character>> board = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Character> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add('.');
+            }
+            board.add(row);
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, res, 0, n);
+
+        return res;
+    }
+}
+
